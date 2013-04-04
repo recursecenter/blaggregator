fec53adadcb7925ba1a0750d949aeec7c5f3c4bf
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-04 14:21:38 -0400

    Add handling of user ids in urls

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,5 @@ from home import views
 
 urlpatterns = patterns('',
     url(r'^login/', views.login, name='login'),
-    url(r'^profile/', views.profile, name='profile'),
+    url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
 )
--- home/views.py
+++ home/views.py
@@ -1,7 +1,9 @@
 from django.http import HttpResponse
 
+from home.models import User
+
 def login(request):
     return HttpResponse("Please log in!")
 
-def profile(request):
-    return HttpResponse("Here is your profile.")
+def profile(request, user_id):
+    return HttpResponse("Here is the profile for user number %s." % user_id)
