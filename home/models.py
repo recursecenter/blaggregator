d2f908db89c1a609f3cf9497a70c3d6342249dcd
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-10 16:06:01 -0400

    Make some profile fields optional

--- home/models.py
+++ home/models.py
@@ -5,10 +5,10 @@ from django.contrib.auth.models import User
 # access with u.hacker.github (where u is user object instance)
 class Hacker(models.Model):
     user = models.OneToOneField(User)
-    avatar_url  = models.CharField(max_length=400)
-    github      = models.CharField(max_length=200)
-    twitter     = models.CharField(max_length=200)
-    irc         = models.CharField(max_length=200)
+    avatar_url  = models.CharField(max_length=400, blank=True)
+    github      = models.CharField(max_length=200, blank=True)
+    twitter     = models.CharField(max_length=200, blank=True)
+    irc         = models.CharField(max_length=200, blank=True)
 
 class Blog(models.Model):
 
