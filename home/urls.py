f981ed40f3360b5e03f35c68719b04759742960c
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-15 15:52:15 -0400

    Redirect requests for root url to /create_account

--- home/urls.py
+++ home/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, url
+from django.http import HttpResponseRedirect
 
 from home import views
 
 urlpatterns = patterns('',
+    url(r'^$', lambda x: HttpResponseRedirect('/create_account')),
     url(r'^log_in/', views.log_in, name='log_in'),
     url(r'^create_account', views.create_account, name='create_account'),
     url(r'^profile/(?P<user_id>\d+)/$', views.profile, name='profile'),
