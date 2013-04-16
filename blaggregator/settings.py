43518945c783f8c34f213011210bbae784fac759
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-16 17:25:23 -0400

    Reconfig static urls

--- blaggregator/settings.py
+++ blaggregator/settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/home/static/'
+STATIC_ROOT = '/static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = '/static/'
+STATIC_URL = ''
 
 # Additional locations of static files
 STATICFILES_DIRS = (
