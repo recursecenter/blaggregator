ac89e6078b28a4eaa3637cd7ce43a720b82d5c7e
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-16 17:35:22 -0400

    Add static urlconf

--- blaggregator/urls.py
+++ blaggregator/urls.py
@@ -14,3 +14,8 @@ urlpatterns = patterns('',
     #url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if not settings.DEBUG:
+    urlpatterns += patterns('',
+        (r'^static/(?P<path>.*)$', 'django.views.static.serve', {'document_root': settings.STATIC_ROOT}),
+    )
