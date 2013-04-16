4d06eb9a11513946631188a20b4c8672adbd4d37
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-16 17:54:48 -0400

    One more conf try

--- blaggregator/settings.py
+++ blaggregator/settings.py
@@ -63,11 +63,11 @@ MEDIA_URL = ''
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = '/static/'
+STATIC_ROOT = ''
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
-STATIC_URL = ''
+STATIC_URL = '/static/'
 
 # Additional locations of static files
 STATICFILES_DIRS = (
