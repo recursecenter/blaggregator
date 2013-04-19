05c9bb5856d8c0740f050d53a8e1956ae07b24d8
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-19 12:13:34 -0400

    Pull 20 random posts

--- home/views.py
+++ home/views.py
@@ -141,7 +141,7 @@ def profile(request, user_id):
 @login_required(login_url='/log_in')
 def new(request):
 
-    postList = list(Post.objects.all().order_by('date_updated').reverse())
+    postList = list(Post.objects.order_by('?')[:20])
 
     for post in postList:
         user = User.objects.get(blog__id__exact=post.blog_id)
