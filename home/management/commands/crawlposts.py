757e0d517524fb83fee1a15d3831c5037b2948db
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-24 17:13:31 -0400

    Add script to crawl all blogs for new posts

---
+++ home/management/commands/crawlposts.py
@@ -0,0 +1,64 @@
+from django.core.management.base import NoArgsCommand, CommandError
+from django.db import transaction
+from django.utils import timezone
+from optparse import make_option
+from home.models import Blog, Post
+from home import feedergrabber27
+
+class Command(NoArgsCommand):
+    help = 'Periodically crawls all blogs for new posts.'
+
+    option_list = NoArgsCommand.option_list + (
+        make_option('--dry-run',
+            action = 'store_true',
+            default = False,
+            help = 'Don\'t actually touch the database',
+            ),
+        )
+
+    @transaction.commit_manually
+    def handle_noargs(self, **options):
+
+        for blog in Blog.objects.all():
+
+            # Feedergrabber returns ( [(link, title, date)], [errors])
+            # We're ignoring the errors returned for right now
+            crawled, _ = feedergrabber27.feedergrabber(blog.feed_url)
+
+            if crawled:
+
+                for link, title, date in crawled:
+
+                    date = timezone.make_aware(date, timezone.get_default_timezone())
+
+                    # create the post instance if it doesn't already exist
+                    post, created = Post.objects.get_or_create(
+                        blog = blog,
+                        url = link,
+                        defaults = {
+                            'title': title,
+                            'date_updated': date,
+                        }
+                    )
+
+                    if created: print "Created", title
+
+                    # if new info, update the posts
+                    if not created:
+                        print "Retrieved", title
+                        updated = False
+                        if date != post.date_updated:
+                            post.date_updated = date
+                            updated = True
+                        if title != post.title:
+                            post.title = title
+                            updated = True
+                        if updated:
+                            print "Updated", title
+                            post.save()
+
+        if options['dry_run']:
+            transaction.rollback()
+            print "\nDON'T FORGET TO RUN THIS FOR REAL\n"
+        else:
+            transaction.commit()
