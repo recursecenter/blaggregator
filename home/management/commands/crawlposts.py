2bb4109cac18b70fa809964d5d1ff88326b4f379
Sasha Laundy <sasha.laundy@gmail.com>
2013-04-25 16:09:45 -0400

    Catch exceptions when crawling blogs

--- home/management/commands/crawlposts.py
+++ home/management/commands/crawlposts.py
@@ -4,6 +4,9 @@ from django.utils import timezone
 from optparse import make_option
 from home.models import Blog, Post
 from home import feedergrabber27
+import logging
+
+log = logging.getLogger(__name__)
 
 class Command(NoArgsCommand):
     help = 'Periodically crawls all blogs for new posts.'
@@ -16,48 +19,55 @@ class Command(NoArgsCommand):
             ),
         )
 
+    def crawlblog(self, blog):
+
+        print "** CRAWLING", blog.feed_url
+
+        # Feedergrabber returns ( [(link, title, date)], [errors])
+        # We're ignoring the errors returned for right now
+        crawled, _ = feedergrabber27.feedergrabber(blog.feed_url)
+
+        if crawled:
+
+            for link, title, date in crawled:
+
+                date = timezone.make_aware(date, timezone.get_default_timezone())
+
+                # create the post instance if it doesn't already exist
+                post, created = Post.objects.get_or_create(
+                    blog = blog,
+                    url = link,
+                    defaults = {
+                        'title': title,
+                        'date_updated': date,
+                    }
+                )
+
+                if created: print "Created", title
+
+                # if new info, update the posts
+                if not created:
+                    print "Retrieved", title
+                    updated = False
+                    if date != post.date_updated:
+                        post.date_updated = date
+                        updated = True
+                    if title != post.title:
+                        post.title = title
+                        updated = True
+                    if updated:
+                        print "Updated", title
+                        post.save()
+
+
     @transaction.commit_manually
     def handle_noargs(self, **options):
 
         for blog in Blog.objects.all():
-
-            print "CRAWLING", blog.feed_url
-
-            # Feedergrabber returns ( [(link, title, date)], [errors])
-            # We're ignoring the errors returned for right now
-            crawled, _ = feedergrabber27.feedergrabber(blog.feed_url)
-
-            if crawled:
-
-                for link, title, date in crawled:
-
-                    date = timezone.make_aware(date, timezone.get_default_timezone())
-
-                    # create the post instance if it doesn't already exist
-                    post, created = Post.objects.get_or_create(
-                        blog = blog,
-                        url = link,
-                        defaults = {
-                            'title': title,
-                            'date_updated': date,
-                        }
-                    )
-
-                    if created: print "Created", title
-
-                    # if new info, update the posts
-                    if not created:
-                        print "Retrieved", title
-                        updated = False
-                        if date != post.date_updated:
-                            post.date_updated = date
-                            updated = True
-                        if title != post.title:
-                            post.title = title
-                            updated = True
-                        if updated:
-                            print "Updated", title
-                            post.save()
+            try:
+                self.crawlblog(blog)
+            except Exception as e:
+                log.exception(e)
 
         if options['dry_run']:
             transaction.rollback()
