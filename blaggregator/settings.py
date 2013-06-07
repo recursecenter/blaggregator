33f8d2621130ed1bdd9d4851a7044b978b159f08
Kenya <kenya.vs@gmail.com>
2013-06-07 18:18:24 -0400

    changed db credentials

--- blaggregator/settings.py
+++ blaggregator/settings.py
@@ -66,8 +66,8 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'blaggregator_dev',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'kenya',
-        'PASSWORD': 'p@55w0rd',
+        'USER': 'sasha',
+        'PASSWORD': '',
         'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
