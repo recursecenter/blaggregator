92f0369c8e2a38b3709f1da516976e85bd760983
Sasha Laundy <sasha.laundy@gmail.com>
2013-06-13 17:06:34 -0400

    Allow custom subdomain

--- blaggregator/settings.py
+++ blaggregator/settings.py
@@ -58,7 +58,7 @@ DATABASES = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost', 'blaggregator.herokuapp.com', 'blaggregator-staging.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', 'blaggregator.herokuapp.com', 'blaggregator-staging.herokuapp.com', 'www.blaggregator.us']
 
 # Local time zone for this installation. Choices can be found here:
 # http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
