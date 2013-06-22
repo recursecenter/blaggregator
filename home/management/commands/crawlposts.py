4b3dceafb62cf7df9da131d7b1257c09407a4bd9
Sasha Laundy <sasha.laundy@gmail.com>
2013-06-21 18:20:48 -0700

    Truncate humbug message subject lines to fit into api char limit

--- home/management/commands/crawlposts.py
+++ home/management/commands/crawlposts.py
@@ -113,12 +113,14 @@ def cleantitle(title):
 
 def send_message_humbug(user, link, title):
 
+    subject = "new blog post: %s" % title
+    subject = subject[:57] + "..."
+
     data = {"type": "stream",
             "to": "%s" % STREAM,
-            "subject": "new blog post: %s" % title,
+            "subject": subject,
             "content": "**%s** has a new blog post: [%s](%s)" % (user.first_name, title, link),
         }
 
     print data['content']
     r = requests.post('https://humbughq-com-y3ee336dh1kn.runscope.net/api/v1/messages', data=data, auth=(email, key))
-    # r = requests.post('https://humbughq.com/api/v1/messages', data=data, auth=(email, key))
