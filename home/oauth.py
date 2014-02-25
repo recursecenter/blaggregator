374d54e356e9fe59dd43034f2cef0ab3e024cdd4
David Albert <davidbalbert@gmail.com>
2014-02-25 18:03:54 -0500

    Add some comments in find_legacy_user

--- home/oauth.py
+++ home/oauth.py
@@ -6,14 +6,27 @@ import sys
 from models import User
 
 def find_legacy_user(strategy, uid, details, user=None, social=None, *args, **kwargs):
+    # user is present if we're currently logged in (very unlikely given there's no
+    # link to /login/hackerschool when you are logged in).
+    #
+    # social is present if we've already oauthed with hackerschool.com before.
+    # If this is the case, we don't need to find the legacy user.
+    # Social.pipeline.social_auth.social_user sets user from social.
     if user or social:
         return None
 
+    # first try finding a legacy account by matching the email returned from
+    # the API. This won't always work because people can change their emails on
+    # hackerschool.com.
     users = User.objects.filter(email=details['email'])
 
     if users:
         return {'user': users[0]}
 
+    # Fall back on matching by username. People can't change thier names on
+    # hackerschool.com. We tried email first because email addresses are
+    # globally unique, while it's possible that two people might have the same
+    # name and thus username.
     users = User.objects.filter(username=details['username'])
 
     if users:
