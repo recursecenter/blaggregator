3a701f9e27b0bf94029f83aad08739de1c7adad3
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-03-04 15:21:55 -0800

    Add simple tests for views that don't need login.

--- home/tests.py
+++ home/tests.py
@@ -1,16 +1,41 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+"""Blaggregator Tests."""
 
-Replace this with more appropriate tests for your application.
-"""
+from unittest import expectedFailure
 
 from django.test import TestCase
+from django.test.client import Client
 
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+class BlaggregatorNoLoginTests(TestCase):
+
+    def test_shows_about_without_login(self):
+        # Given
+        client = Client()
+
+        # When
+        response = client.get('/about/')
+
+        # Then
+        self.assertEqual(200, response.status_code)
+
+    @expectedFailure
+    def test_404_for_unknown_slug(self):
+        # Given
+        client = Client()
+
+        # When
+        response = client.get('/post/BOGUS/view')
+
+        # Then
+        self.assertEqual(404, response.status_code)
+
+    def test_should_prompt_login(self):
+        # Given
+        client = Client()
+
+        # When
+        response = client.get('/login/')
+
+        # Then
+        self.assertEqual(200, response.status_code)
+        self.assertIn('Log in with Hacker School', response.content)
