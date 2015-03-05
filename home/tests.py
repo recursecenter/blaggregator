4757ff0c86a675d836884c9a7497be9adc0cb2a1
Puneeth Chaganti <punchagan@muse-amuse.in>
2015-03-04 22:46:14 -0800

    Add a test case class for logged in view tests.

--- home/tests.py
+++ home/tests.py
@@ -2,15 +2,19 @@
 
 from unittest import expectedFailure
 
+from django.contrib.auth.models import User
 from django.test import TestCase
 from django.test.client import Client
+from django.test.utils import override_settings
+
+from home.oauth import create_user, HackerSchoolOAuth2, create_or_update_hacker
 
 
 class BlaggregatorNoLoginTests(TestCase):
 
     def test_shows_about_without_login(self):
         # Given
-        client = Client()
+        client = self.client
 
         # When
         response = client.get('/about/')
@@ -21,7 +25,7 @@ class BlaggregatorNoLoginTests(TestCase):
     @expectedFailure
     def test_404_for_unknown_slug(self):
         # Given
-        client = Client()
+        client = self.client
 
         # When
         response = client.get('/post/BOGUS/view')
@@ -31,11 +35,57 @@ class BlaggregatorNoLoginTests(TestCase):
 
     def test_should_prompt_login(self):
         # Given
-        client = Client()
+        client = self.client
 
         # When
         response = client.get('/login/')
 
         # Then
         self.assertEqual(200, response.status_code)
-        self.assertIn('Log in with Hacker School', response.content)
+        self.assertIn('Login with Hacker School', response.content)
+
+@override_settings(
+    AUTHENTICATION_BACKENDS=(
+        'django.contrib.auth.backends.ModelBackend',
+    ),
+)
+class BlaggregatorLoggedInTests(TestCase):
+
+    @classmethod
+    def setUpClass(cls):
+        User.objects.create_user('test_user')
+        test_user = User.objects.get(username='test_user')
+        test_user.set_password('test_user')
+        test_user.save()
+
+    def setUp(self):
+        self.client.login(username='test_user', password='test_user')
+
+    # Tests ####
+
+    def test_should_redirect_logged_in_user_to_new(self):
+        # Given
+        client = self.client
+
+        # When
+        response = client.get('/login/')
+
+        # Then
+        self.assertRedirectPath(response, 'new')
+
+    def test_logout_redirects_to_home(self):
+        # Given
+        client = self.client
+
+        # When
+        response = client.get('/logout/')
+
+        # Then
+        self.assertRedirectPath(response, '/')
+
+    # Assertions ####
+
+    def assertRedirectPath(self, response, path, redirect=302):
+        self.assertEqual(redirect, response.status_code)
+        actual_path = response.get('Location', '').rsplit('/', 1)[-1]
+        self.assertEqual(path.strip('/'), actual_path)
