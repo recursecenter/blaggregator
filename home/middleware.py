c4037b5106ede7b051ee18db7cd26b6aa828943d
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-10-17 23:29:05 +0530

    Redirect to https URL directly

--- home/middleware.py
+++ home/middleware.py
@@ -4,7 +4,7 @@ from django import http
 class RecurseSubdomainMiddleware(object):
     """Middleware to redirect all users to recurse subdomain."""
 
-    REDIRECTS = {
+    HTTPS_REDIRECTS = {
         'blaggregator.us': 'blaggregator.recurse.com',
         'www.blaggregator.us': 'blaggregator.recurse.com',
     }
@@ -13,7 +13,7 @@ class RecurseSubdomainMiddleware(object):
         """Check for old HTTP_HOST and redirect to recurse subdomain."""
 
         host = request.get_host()
-        if host in self.REDIRECTS:
-            request.META['HTTP_HOST'] = self.REDIRECTS[host]
-            newurl = request.build_absolute_uri()
+        if host in self.HTTPS_REDIRECTS:
+            request.META['HTTP_HOST'] = self.HTTPS_REDIRECTS[host]
+            newurl = request.build_absolute_uri().replace('http://', 'https://')
             return http.HttpResponsePermanentRedirect(newurl)
