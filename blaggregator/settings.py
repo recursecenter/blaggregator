9ecd83f87ae0d1285f741946fb475e4063bda64e
Puneeth Chaganti <punchagan@muse-amuse.in>
2016-10-21 03:49:46 +0530

    Add some basic tests for the atom feed view

--- blaggregator/settings.py
+++ blaggregator/settings.py
@@ -1,4 +1,5 @@
 import os
+import sys
 SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
 
 # True: heroku config:set DJANGO_DEBUG=True
@@ -53,6 +54,14 @@ DATABASES = {
         'PORT': '',                      # Set to empty string for default.
     }
 }
+
+if 'test' in sys.argv:
+    DATABASES = {
+        'default': {
+            'NAME': 'blaggregator_test',
+            'ENGINE': 'django.db.backends.sqlite3',
+        }
+    }
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
