ba9e28494fe34d8d4f54a51e99f6b6e1a8720f69
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-03-18 12:01:40 +0530

    Format views.py using black

--- home/views.py
+++ home/views.py
@@ -23,9 +23,7 @@ from home.oauth import update_user_details
 from home.feeds import LatestEntriesFeed
 import feedergrabber27
 
-EXISTING_FEED_MESSAGE = (
-    'This feed has already been added!'
-)
+EXISTING_FEED_MESSAGE = ('This feed has already been added!')
 NO_CONTENT_MESSAGE = (
     'Could not fetch feed from <a href="{url}">{url}</a>. Is the website up?'
 )
@@ -43,6 +41,7 @@ SUCCESS_MESSAGE = (
 
 
 def ensure_blog_exists(f):
+
     @wraps(f)
     def wrapper(request, blog_id):
         try:
@@ -50,19 +49,24 @@ def ensure_blog_exists(f):
             request.blog = blog
         except Blog.DoesNotExist:
             raise Http404
+
         return f(request, blog_id)
+
     return wrapper
 
 
 def ensure_hacker_exists(f):
+
     @wraps(f)
     def wrapper(request, user_id):
         try:
             hacker = Hacker.objects.get(user=user_id)
         except Hacker.DoesNotExist:
             raise Http404
+
         request.hacker = hacker
         return f(request, user_id)
+
     return wrapper
 
 
@@ -76,7 +80,6 @@ def paginator(queryset, page_number, page_size=30):
     except EmptyPage:
         # If page is out of range (e.g. 9999), deliver last page of results.
         items = paginator.page(paginator.num_pages)
-
     return items
 
 
@@ -87,10 +90,8 @@ def view_post(request, slug):
     useful things with it.
 
     """
-
     try:
         post = Post.objects.get(slug=slug)
-
     except Post.DoesNotExist:
         raise Http404('Post does not exist.')
 
@@ -107,6 +108,7 @@ def view_post(request, slug):
 def log_in_oauth(request):
     if request.user.is_authenticated():
         return HttpResponseRedirect(reverse('new'))
+
     else:
         return render(request, 'home/log_in_oauth.html')
 
@@ -121,18 +123,17 @@ def log_out(request):
 @login_required
 def add_blog(request):
     ''' Adds a new blog to a user's profile. '''
-
     if request.method == 'GET':
-        return HttpResponseRedirect(reverse('profile', kwargs={'user_id': request.user.id}))
+        return HttpResponseRedirect(
+            reverse('profile', kwargs={'user_id': request.user.id})
+        )
 
     feed_url = request.POST.get('feed_url', None)
     if feed_url:
         feed_url = feed_url.strip()
-
         # add http:// prefix if missing
         if feed_url[:4] != "http":
             feed_url = "http://" + feed_url
-
         # pull out human-readable url from feed_url
         # (naively - later we will crawl blog url for feed url)
         if re.search('atom.xml/*$', feed_url):
@@ -141,25 +142,33 @@ def add_blog(request):
             url = re.sub('rss/*$', '', feed_url)
         else:
             url = feed_url
-
         # janky short circuit if they've already added this url
         if Blog.objects.filter(user=request.user.id, url=url).exists():
             Blog.objects.filter(feed_url=feed_url).update(skip_crawl=False)
             messages.info(request, EXISTING_FEED_MESSAGE)
-            return HttpResponseRedirect(reverse('profile', kwargs={'user_id': request.user.id}))
+            return HttpResponseRedirect(
+                reverse('profile', kwargs={'user_id': request.user.id})
+            )
 
         contents, errors = feedergrabber27.retrieve_file_contents(feed_url)
         if contents is None:  # URLError or HTTPError
-            messages.error(request, NO_CONTENT_MESSAGE.format(url=feed_url), extra_tags='safe')
-
+            messages.error(
+                request,
+                NO_CONTENT_MESSAGE.format(url=feed_url),
+                extra_tags='safe',
+            )
         elif (
-                contents.bozo and
-                not isinstance(contents.bozo_exception, feedergrabber27.CharacterEncodingOverride)
+            contents.bozo
+            and not isinstance(
+                contents.bozo_exception,
+                feedergrabber27.CharacterEncodingOverride,
+            )
         ):  # Content failed to parse
             guessed_url = feedergrabber27.find_feed_url(contents)
             message = SUGGEST_FEED_URL_MESSAGE if guessed_url is not None else INVALID_FEED_MESSAGE
-            messages.error(request, message.format(url=guessed_url), extra_tags='safe')
-
+            messages.error(
+                request, message.format(url=guessed_url), extra_tags='safe'
+            )
         else:
             # create new blog record in db
             Blog.objects.create(
@@ -167,22 +176,27 @@ def add_blog(request):
                 feed_url=feed_url,
                 url=url,
             )
-            messages.success(request, SUCCESS_MESSAGE.format(url=feed_url), extra_tags='safe')
-
+            messages.success(
+                request,
+                SUCCESS_MESSAGE.format(url=feed_url),
+                extra_tags='safe',
+            )
     else:
         messages.error(request, "No feed URL provided.")
-
-    return HttpResponseRedirect(reverse('profile', kwargs={'user_id': request.user.id}))
+    return HttpResponseRedirect(
+        reverse('profile', kwargs={'user_id': request.user.id})
+    )
 
 
 @login_required
 @ensure_blog_exists
 def delete_blog(request, blog_id):
-
     blog = request.blog
     user = request.user
     blog.delete()
-    return HttpResponseRedirect(reverse('profile', kwargs={'user_id': user.id}))
+    return HttpResponseRedirect(
+        reverse('profile', kwargs={'user_id': user.id})
+    )
 
 
 @login_required
@@ -195,7 +209,9 @@ def edit_blog(request, blog_id):
     if form.is_valid():
         form.instance.skip_crawl = False
         form.save()
-    return HttpResponseRedirect(reverse('profile', kwargs={'user_id': user.id}))
+    return HttpResponseRedirect(
+        reverse('profile', kwargs={'user_id': user.id})
+    )
 
 
 @login_required
@@ -203,9 +219,7 @@ def edit_blog(request, blog_id):
 def profile(request, user_id):
     added_blogs = Blog.objects.filter(user=user_id)
     owner = True if int(user_id) == request.user.id else False
-
     post_list = Post.objects.filter(blog__user=user_id)
-
     context = {
         'hacker': request.hacker,
         'owner': owner,
@@ -219,15 +233,11 @@ def profile(request, user_id):
 @login_required
 def new(request):
     ''' Newest blog posts - main app view. '''
-
     posts = Post.objects.all()
     page = request.GET.get('page', 1)
     post_list = paginator(posts, page)
-
     context = {
-        "post_list": post_list,
-        'show_avatars': True,
-        "page_view": "new"
+        "post_list": post_list, 'show_avatars': True, "page_view": "new"
     }
     return render(request, 'home/new.html', context)
 
@@ -235,21 +245,18 @@ def new(request):
 @login_required
 def search(request):
     ''' Search blog posts based on query - main app view. '''
-
     query = request.GET.get('q', '')
     posts = Post.objects.filter(title__search=query)
     count = posts.count()
     page = request.GET.get('page', 1)
     post_list = paginator(posts, page)
-
     context = {
         "count": count,
         "query": query,
         "post_list": post_list,
         'show_avatars': True,
-        "page_view": "search"
+        "page_view": "search",
     }
-
     return render(request, 'home/search.html', context)
 
 
@@ -265,7 +272,6 @@ def updated_avatar(request, user_id):
 # create/delete signals.  Probably most other views could be cached.
 def feed(request):
     """Atom feed of new posts."""
-
     token = request.GET.get('token')
     if authenticate(token=token) is None:
         raise Http404
@@ -276,11 +282,9 @@ def feed(request):
 @login_required
 def refresh_token(request):
     """Refresh a users' auth token."""
-
     hacker = Hacker.objects.get(user=request.user)
     hacker.token = uuid.uuid4().hex
     hacker.save()
-
     profile_url = reverse('profile', kwargs={'user_id': request.user.id})
     return HttpResponseRedirect(profile_url)
 
@@ -304,16 +308,16 @@ def most_viewed(request, ndays='7'):
     since = now - datetime.timedelta(days=ndays)
     entries = _get_most_viewed_entries(since=since)
     streams = dict(STREAM_CHOICES)
-
     # Return a tab separated values file, if requested
     if request.GET.get('tsv') == '1':
         header = 'post_id\ttitle\turl\tcount\n'
         text = '\n'.join(_get_tsv(entry) for entry in entries)
         text = header + text
         response = HttpResponse(text, content_type='text/tab-separated-values')
-
     else:
-        Post = namedtuple('Post', ('authorid', 'avatar', 'slug', 'title', 'stream'))
+        Post = namedtuple(
+            'Post', ('authorid', 'avatar', 'slug', 'title', 'stream')
+        )
         context = {
             'post_list': [
                 Post(
@@ -330,27 +334,26 @@ def most_viewed(request, ndays='7'):
             'show_avatars': True,
         }
         response = render(request, 'home/most_viewed.html', context)
-
     return response
 
 
 def _get_most_viewed_entries(since, n=20):
     # Get posts visited during the last week
     entries = LogEntry.objects.filter(date__gte=since)
-
     # Get post url and title
     entries = entries.values(
-        'post__id', 'post__title', 'post__url', 'post__slug',
-        'post__blog__user__id', 'post__blog__user__hacker__avatar_url',
+        'post__id',
+        'post__title',
+        'post__url',
+        'post__slug',
+        'post__blog__user__id',
+        'post__blog__user__hacker__avatar_url',
         'post__blog__stream',
     )
-
     # Count the visits
     entries = entries.annotate(total=Count('post__id'))
-
     # Get top 'n' posts
     entries = entries.order_by('total').reverse()[:n]
-
     return entries
 
 
@@ -363,6 +366,6 @@ BlogForm = modelform_factory(
     fields=("feed_url", "stream"),
     widgets={
         'feed_url': TextInput(attrs={'class': 'form-control', 'type': 'url'}),
-        'stream': Select(attrs={'class': 'custom-select'})
-    }
+        'stream': Select(attrs={'class': 'custom-select'}),
+    },
 )
