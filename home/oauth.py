40b06beb99893399b97f59250cc438f28ebc7e2c
Puneeth Chaganti <punchagan@muse-amuse.in>
2018-03-18 10:09:15 +0530

    Remove code to find legacy user
    There are no more legacy users around

--- home/oauth.py
+++ home/oauth.py
@@ -13,38 +13,6 @@ USER_FIELDS = ['username', 'email']
 log = logging.getLogger("blaggregator")
 
 
-def find_legacy_user(strategy, uid, details, user=None, social=None, *args, **kwargs):
-    # user is present if we're currently logged in (very unlikely given there's no
-    # link to /login/hackerschool when you are logged in).
-    #
-    # social is present if we've already oauthed with hackerschool.com before.
-    # If this is the case, we don't need to find the legacy user.
-    # Social.pipeline.social_auth.social_user sets user from social.
-    if user or social:
-        return None
-
-    # first try finding a legacy account by matching the email returned from
-    # the API. This won't always work because people can change their emails on
-    # hackerschool.com.
-    users = User.objects.filter(email=details['email'])
-
-    if users:
-        return {'user': users[0]}
-
-    # Fall back on matching by username. People can't change thier names on
-    # hackerschool.com. We tried email first because email addresses are
-    # globally unique, while it's possible that two people might have the same
-    # name and thus username.
-    users = User.objects.filter(username=details['username'])
-
-    if users:
-        return {'user': users[0]}
-
-    # If we get down here, we're almost certainly dealing with a new uesr.
-    # Social.pipeline.user.create_user will make a new user shortly after this.
-    return None
-
-
 def create_user(strategy, details, response, uid, user=None, *args, **kwargs):
     if user:
         return
