d9e1a62f17c214169cdcff5c8d583128113bbb8b
Puneeth Chaganti <punchagan@muse-amuse.in>
2021-09-17 19:13:11 +0530

    deps: Upgrade django-storages to the latest version
    Upgrade to a version that works with the current version of Django. The
    previous version doesn't work with the updated version of Django, but it
    doesn't cause an issue with running the code locally, since the S3 storage is
    only used in staging or production.
    
    Running `python manage.py collectstatic --noinput` fails when the S3 backend is
    set. This is run on Heroku deployments. It may be worth adding a test locally
    to detect this, without needing to deploy.

--- blaggregator/settings/production.py
+++ blaggregator/settings/production.py
@@ -18,9 +18,7 @@ AWS_ACCESS_KEY_ID = os.environ["AWS_ACCESS_KEY_ID"]
 AWS_SECRET_ACCESS_KEY = os.environ["AWS_SECRET_ACCESS_KEY"]
 AWS_STORAGE_BUCKET_NAME = "blaggregator"
 STATIC_URL = "http://" + AWS_STORAGE_BUCKET_NAME + ".s3.amazonaws.com/"
-STATICFILES_STORAGE = "storages.backends.s3boto.S3BotoStorage"
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto.S3BotoStorage"
+STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
+DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 # Social Auth
-SOCIAL_AUTH_HACKERSCHOOL_REDIRECT_URL = (
-    "http://www.blaggregator.us/complete/hackerschool"
-)
+SOCIAL_AUTH_HACKERSCHOOL_REDIRECT_URL = "http://www.blaggregator.us/complete/hackerschool"
